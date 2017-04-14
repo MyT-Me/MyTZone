255ea14a7172fb440840e27887fed1f44857c199
Revanth Penugonda <revanthpenugonda@gmail.com>
2017-04-14 05:28:36 -0700

    Little Update on Add Methods

--- models/addMethods.js
+++ models/addMethods.js
@@ -10,3 +10,71 @@ exports.createUser = function(req,callback) {
         callback(err);
     })
 }
+
+exports.addEducation = function(req,callback){
+
+}
+
+exports.addWorkExperience = function(req,callback){
+
+}
+
+exports.addWorkSection = function(req,callback){
+
+}
+
+exports.addCertificates = function(req,callback){
+
+}
+exports.addTakingClasses = function(req,callback){
+
+}
+
+exports.addConductingClasses = function(req,callback){
+
+}
+
+exports.addMentoring = function(req,callback){
+
+}
+
+exports.addWritings = function(req,callback){
+
+}
+
+exports.addConfrences = function(req,callback){
+
+}
+
+exports.addAwards = function(req,callback){
+
+}
+
+exports.addRecognizedExperteise = function(req,callback){
+
+}
+
+exports.addPatents = function(req,callback){
+
+}
+
+exports.addLanguages = function(req,callback){
+
+}
+
+exports.addLeisureTravel = function(req,callback){
+
+}
+
+exports.addTools = function(req,callback){
+
+}
+
+exports.addSkills = function(req,callback){
+
+}
+
+exports.addPoints = function(req,callback){
+
+}
+    
