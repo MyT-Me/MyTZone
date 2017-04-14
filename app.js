a2eecf08fe3aa0eb03a5e24b4ea63a1f9ca79a10
Revanth Penugonda <revanthpenugonda@gmail.com>
2017-04-14 08:41:34 -0700

    CORS ENABLED

--- app.js
+++ app.js
@@ -26,6 +26,14 @@ mongoose.connect(dbconfig.getMongoUrl()).then(
 });
 
 
+//Enabling CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.get('/',function (req,res) {
     console.log("Request");
     res.render('welcome');
